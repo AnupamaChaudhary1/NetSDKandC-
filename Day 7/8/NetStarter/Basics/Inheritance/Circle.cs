9fa2f76491555784d6cb3db1978e60830232aa44
AnupamaChaudhary1 <anupamachaudhary183@gmail.com>
2024-04-14 09:05:10 +0545

    new

---
+++ Day 7/8/NetStarter/Basics/Inheritance/Circle.cs
@@ -0,0 +1,10 @@
+class Circle : I2Dshape
+{
+    public Circle(double r)
+    {
+        radius = r;
+    }
+    double radius;
+    public double GetArea() => Math.PI * radius * radius;
+    public double GetPerimeter() => 2 * Math.PI * radius;
+}
---
+++ Day 7/8/NetStarter/Basics/Inheritance/I2Dshape.cs
@@ -0,0 +1,5 @@
+public interface I2Dshape
+{
+    public double GetArea();
+    public double GetPerimeter();
+}
