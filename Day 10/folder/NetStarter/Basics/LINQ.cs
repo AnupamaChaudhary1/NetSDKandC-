15aa8f0a2755d4016fd1b22a481b36f1ec1ae062
AnupamaChaudhary1 <anupamachaudhary183@gmail.com>
2024-04-19 10:28:39 +0545

    address

--- Day 10/folder/NetStarter/Basics/LINQ.cs
+++ Day 10/folder/NetStarter/Basics/LINQ.cs
@@ -6,6 +6,13 @@ class LINQ
     int[] ages = [23, 1, 5, 67, 98, 56, 78];
     //Find square of all numbers in ages collection
     //imparative
+    List<Student> students = [
+        new Student() { name = "Ram", address = "Ktm"},
+        new Student() { name = "Laxman", address = "Dang"},
+        new Student() { name = "Bikendra", address = "Ktm"},
+        new Student() { name = "Sandip", address = "Dang"},
+        new Student() { name = "Chahana", address = "Dhangadhi"},
+    ];
     public void LearnLinq()
     {
         List<int> squares = [];
@@ -16,15 +23,19 @@ class LINQ
         }
 
         //Declarative
-        var squares = ages.Select(ages => ages * age);
+        var squares = ages.Select(ages => ages * ages); // method syntax
+        var squaresAlternate = from agr in ages //expression
+                               selectage* ages;
         //Find odd numbers within ages
         var odds = ages.Where(x => x % 2 == 1);
         //find cubes of all even numbers within ages 
-
-
         var cubesOfEvenNumbers = ages
-        .Where(age => age % 2 == 0) // Filter even numbers
-        .Select(age => age * age * age);
+        .Where(ages => ages % 2 == 0) // Filter even numbers
+        .Select(ages => ages * ages * ages);
+
+        //find students who live in dang
+
+        var inDang = students.Where(students => students.address == "Dang");
 
     }
 }
